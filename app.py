7c7e100cf7b2ed7517583feda4e6b38ba1050573
realakshay <gaikwadakshay663@gmail.com>
2020-04-20 16:49:03 +0530

    error resolving by creating run file

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///data.db'
 api=Api(app)
 jwt = JWT(app,authenticate,identity)
 
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item,"/item/<string:name>")
 api.add_resource(ItemList,"/items")
