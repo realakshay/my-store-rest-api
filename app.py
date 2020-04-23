f17bc9cc9f20ae7a03c3f58afa026793b39ea831
realakshay <gaikwadakshay663@gmail.com>
2020-04-23 11:01:28 +0530

    adding postgres environment variable and psycopg2

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from resources.store import Store, StoreList
 app=Flask(__name__)
 app.secret_key='Akshay'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URI''sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL','sqlite:///data.db')
 api=Api(app)
 jwt = JWT(app,authenticate,identity)
 
